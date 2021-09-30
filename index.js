d0975a944c71f776cf508cf3410e842201b9aa4a
Antonio Barrera <jeantonio.barrera@gmail.com>
2021-09-30 14:12:00 -0600

    hide second input feature added

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 const billAmount = document.querySelector("#bill-amount");
-const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
+const cashLabel = document.querySelector("#cash-label");
+const cashGiven = document.querySelector("#cash-given");
+const nextButton = document.querySelector("#next-button");
 const message = document.querySelector("#error-message");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
 
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
-checkButton.addEventListener("click", function validateBillAndCashAmount(){
-    hideMessage();
+hideElements();
+hideMessage();
+
+nextButton.addEventListener("click", function validateBillAmount() {
     if(billAmount.value > 0) {
-        if(cashGiven.value >= billAmount.value) {
-            const amountToBeReturned = cashGiven.value - billAmount.value;
-            calculateChange(amountToBeReturned);
-        } else {
-            showMessage("The cash provided is not enough!");
-        }
+        showElements();
+        hideMessage();
     } else {
-        showMessage("Invalid bill amount!");
+        hideElements();
+        showMessage("Invalid input. It must be greater than 0.");
+    }
+})
+
+checkButton.addEventListener("click", function validateCashAmount(){
+    if(cashGiven.value >= billAmount.value) {
+        const amountToBeReturned = cashGiven.value - billAmount.value;
+        calculateChange(amountToBeReturned);
+        hideMessage();
+    } else {
+        showMessage("The cash provided is not enough!");
     }
 });
 
@@ -37,4 +48,16 @@ function hideMessage() {
 function showMessage(msg) {
     message.style.display = "block";
     message.innerText = msg;
+}
+
+function hideElements() {
+    cashLabel.style.display = "none";
+    cashGiven.style.display = "none";
+    checkButton.style.display = "none";
+}
+
+function showElements() {
+    cashLabel.style.display = "block";
+    cashGiven.style.display = "block";
+    checkButton.style.display = "block";
 }
